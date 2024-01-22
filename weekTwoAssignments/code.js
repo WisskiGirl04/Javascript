8c742474873feb627da5485b75b7b76740f26397
CrystaLynn Wisniewski <CWISNIEWSKI85911@uat.edu>
2024-01-22 02:44:33 -0500

    Added an array of names in js file and random generation to pick a name for that game session

--- weekTwoAssignments/code.js
+++ weekTwoAssignments/code.js
@@ -1,4 +1,12 @@
-function myFunction(){
-    document.getElementById("numberSlot").innerHTML = "Why not?";
+let names = ["Mathew", "Alice", "Jordan", "Norman", "Horton"];
+let chosenName;
+let counter = 0;
+function generateName(){
+    chosenName = names[(Math.floor(Math.random() * names.length))];
+    document.getElementById("clickTheButtonText").style.display = "none";
+    document.getElementById("generateButton").style.display = "none";
+    console.log("Chosen name = " + chosenName);
+    console.log(names[(Math.floor(Math.random() * names.length))]);
+    <!--"document.getElementById('numberSlot').innerHTML = chosenName;">
     
 }
