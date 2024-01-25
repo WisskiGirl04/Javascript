1d4bbb57626b81bf00f343f4a8f9b07744f82204
CrystaLynn Wisniewski <CWISNIEWSKI85911@uat.edu>
2024-01-25 01:54:46 -0500

    adjusting looks and adding input stuff

--- weekTwoAssignments/code.js
+++ weekTwoAssignments/code.js
@@ -1,10 +1,16 @@
 let names = ["Lucas", "Alice", "Jordan", "Norman", "Horton"];
 let chosenName;
 let counter = 0;
-function generateName(){
+
+function beginGame(){
     chosenName = names[(Math.floor(Math.random() * names.length))];
     document.getElementById("clickTheButtonText").style.display = "none";
     document.getElementById("generateButton").style.display = "none";
+    document.getElementById("name").style.visibility = "visible";
+    document.getElementById("letterInput").style.visibility = "visible";
+    document.getElementById("checkButton").style.visibility = "visible";
+    document.getElementById("instructions").style.visibility = "visible";
+    document.getElementById("incorrectLetters").style.visibility = "visible";
     console.log("Chosen name = " + chosenName);
     console.log(names[(Math.floor(Math.random() * names.length))]);
     <!--"document.getElementById('numberSlot').innerHTML = chosenName;">
